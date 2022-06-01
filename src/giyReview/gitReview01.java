da451a990aef507320999e51a5ce95de0d69ddb7
MaksigBor007 <amazonmaksig@gmail.com>
2022-05-31 19:35:43 -0600

    first commit

---
+++ src/giyReview/gitReview01.java
@@ -0,0 +1,29 @@
+package giyReview;
+
+public class gitReview01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Today is lovely day");
+		System.out.println("Because I have learnt git hub ");
+		
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+}
+
+}
